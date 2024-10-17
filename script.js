f831d75eb72ece609f031acb2f127d6cf3ef6a71
FL <felip3.fl@gmail.com>
2024-10-16 22:34:59 -0300

    remove javascript code

--- script.js
+++
@@ -1,6 +0,0 @@
-function showSelectedValue() {
-    const selectElement = document.getElementById('items');
-    const messageElement = document.getElementById('message');
-    const selectedValue = selectElement.value;
-    messageElement.textContent = `Você selecionou: ${selectedValue}`;
-}
